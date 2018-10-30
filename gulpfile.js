0230b335cca0116239acab16714e8ed07804f1cb
Andrej <b.andrej@cumir.de>
2018-10-31 00:42:22 +0100

    - Adjusted gulp script, using pump now, to localise errors faster

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,58 @@
 const gulp = require('gulp');
 const riot = require('gulp-riot');
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify')
-const rename = require('gulp-rename')
+const concat = require('gulp-concat');
+//const uglify = require('gulp-uglify');
+//const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
+const pump = require('pump');
 
-gulp.task('riot', () => {
-    gulp.src('src/components/*.tag')
-        .pipe(riot({
+gulp.task('riot', (cb) => {
+    pump([
+        gulp.src('src/components/*.tag'),
+        riot({
             compact: true
-          }))
-        .pipe(concat('components_all.js'))
+        }),
+        concat('components_all.js'),
         // .pipe(gulp.dest('dest/components'))
         // .pipe(rename('components_all.min.js'))
         // .pipe(uglify())
-        .pipe(gulp.dest('dest/components'));
+        gulp.dest('dest/components')
+    ],
+    cb        
+    );
 });
 
 gulp.task('process_js', () => {
     // Consider to run uglify/minify over JS files later
-    gulp.src(['src/js/**/*']).pipe(gulp.dest('dest/js'));
+    return gulp.src(['src/js/**/*']).pipe(gulp.dest('dest/js'));
 });
 
 gulp.task('process_css', () => {
     // Consider to run LESS/SASS/minifier processor over CSS files later
-    gulp.src(['src/css/**/*']).pipe(gulp.dest('dest/css')).pipe(browserSync.stream());
+    return gulp.src(['src/css/**/*']).pipe(gulp.dest('dest/css')).pipe(browserSync.stream());
 });
 
 const static_paths = ['./src/*.html', './src/*.json', './src/icon/**/*', './src/json/**/*', './src/img/**/*'];
 
-gulp.task('copy_static', () => {
-    gulp.src(['./src/*.html']).pipe(gulp.dest('dest/'));
-    gulp.src(['./src/*.json']).pipe(gulp.dest('dest/'));
-    gulp.src(['./src/icon/**/*']).pipe(gulp.dest('dest/icon'));
-    gulp.src(['./src/json/**/*']).pipe(gulp.dest('dest/json'));
-    gulp.src(['./src/img/**/*']).pipe(gulp.dest('dest/img'));
+gulp.task('copy_static', (cb) => {
+    pump([
+        gulp.src(['./src/*.html', './src/*.json']), gulp.dest('./dest/'),
+        gulp.src(['./src/icon/**/*']), gulp.dest('./dest/icon/'),
+        gulp.src(['./src/font/**/*']), gulp.dest('./dest/font/'),
+        gulp.src(['./src/json/**/*']), gulp.dest('./dest/json/'),
+        gulp.src(['./src/img/**/*']), gulp.dest('./dest/img/')
+    ],
+    cb
+    );
 });
 
 gulp.task('browser-sync', function () {
-	browserSync.init({
-		injectChanges: true,
-		server: {
-			baseDir: ['./dest']
-		}
-	});
+    return browserSync.init({
+        injectChanges: true,
+        server: {
+            baseDir: ['./dest']
+        }
+    });
 });
 
 gulp.task('watch', ['browser-sync'], () => {
@@ -53,8 +62,6 @@ gulp.task('watch', ['browser-sync'], () => {
     gulp.watch('./src/css/**/*', ['process_css']);
 });
 
-gulp.task('run_dev', ['default', 'watch'], () => {
-});
+gulp.task('run_dev', ['default', 'watch']);
 
-gulp.task('default', ['riot', 'process_css', 'process_js', 'copy_static'], () => {
-});
+gulp.task('default', ['riot', 'process_css', 'process_js', 'copy_static']);
